30bd97df6f2eda0015cfd3432eb821eb5b2f2cd2
unknown <rostykvoznyak@gmail.com>
2020-10-27 23:34:37 +0200

    Lesson2Homework3

---
+++ Lesson2Homework3/Lesson2Homework3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson2Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 5;
+            int i = 8;
+            double f=(x+y+i)/3.0;
+            Console.WriteLine($"Avg={f}");
+        }
+    }
+}
